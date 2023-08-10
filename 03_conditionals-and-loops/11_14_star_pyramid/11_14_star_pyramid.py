b1453c784984f4c56399f6dbeb161f89eced3b10
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2023-08-10 11:52:47 +0900

    removed english files accidentally left in

--- 03_conditionals-and-loops/11_14_star_pyramid/11_14_star_pyramid.py
+++ 03_conditionals-and-loops/11_14_star_pyramid/11_14_star_pyramid.py
@@ -1,5 +1,5 @@
-# Write a script that prints a star pyramid of flexible size
-# If the `stars` variable is `5`, your code will output:
+# 星マークを使ってピラミッドを自由な大きさで出力するスクリプトを記述します
+# 変数`stars`が`5`の場合、コードの出力は以下のようになります。
 #
 # *
 # * *
@@ -7,18 +7,18 @@
 # * * * *
 # * * * * * 
 #
-# There should be five rows in total:
-# 1. the 1st row will have 1 star,
-# 2. the 2nd row will have 2 stars,
-# 3. the 3rd row will have 3 stars,
-# 4. the 4th row will have 4 stars,
-# 5. the 5th row will have 5 stars
+# 全部で5つの行があります。
+# 1. 1行目は星が1個あります。
+# 2. 2行目は星が2個あります。
+# 3. 3行目は星が3個あります。
+# 4. 4行目は星が4個あります。
+# 5. 5行目は星が5個あります。
 #
-# Another example: if you set the `stars` variable tp `3`, 
-# your code will output:
+# 別の例: `stars`変数を`3`にした場合、
+# コードの出力は以下のようになります。
 #
 # *
 # * *
 # * * *
 #
-# HINT: Think of nested for loops!
+# ヒント:入れ子になったforループを考えてみてください。
--- 04_user-input-string-formatting/12_12_formattme/12_13_formattme.py
+++ 04_user-input-string-formatting/12_12_formattme/12_13_formattme.py
@@ -1,11 +1,11 @@
-# Fix the wikipedia blurb below by replacing the value for `animal`
-# with the name of the animal that the text is talking about.
-# Then, change the that way you're displaying the multi-line string
-# so that the output doesn't show any superfluous spacing.
+# 次のWikipediaの記事について、`animal`の値を
+# このテキストが説明している動物の名前で置き換えてください。
+# さらに、複数行文字列の表示方法を変更し、
+# 出力に余計な空白が含まれないようにしてください。
 
 animal = None
 blurb = f"The {animal} (Felis {animal}us) is a domestic species of small \
-        carnivorous mammal. It is the only domesti{animal}ed species \
+        carnivorous mammal.It is the only domesti{animal}ed species \
         in the family Felidae and is often referred to as the \
         domestic {animal} to distinguish it from the wild members of the family."
 
