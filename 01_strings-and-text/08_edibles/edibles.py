3f1e52d5ac6ce487b04ad618a2895247cca9d71a
Yuma Sumi <yuma.sumi@woven-planet.global>
2023-08-22 21:09:48 +0900

    removed old folders

--- 01_strings-and-text/08_edibles/edibles.py
+++
@@ -1,8 +0,0 @@
-# 次の文から食材の名前を4つ抽出してください。
-# 抽出には文字列スライシングのみを使用します。
-# 簡単に位置を確かめられるように、ペンと紙を
-# 使用してインデックス番号を付けても構いません。
-#
-# これらの食材からどのような料理を作れますか?
-
-s = "They grappled with their leggins before going to see the buttercups flourish."
